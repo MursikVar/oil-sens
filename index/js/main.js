7420b1f899424e6f2e158a1c150a73421d658243
Vary Mark <var.mark@bk.ru>
2024-05-16 02:32:53 +0700

    aaa

---
+++ index/js/main.js
@@ -0,0 +1,10 @@
+console.log("скрипт подключен")
+// $(function()
+// {
+//     let mixer = mixitup('.directions__list');
+
+//     $('.specifications__card-btn').on('click', function () {
+//         $('.specifications__card-btn').removeClass('specifications__card-img--active')
+//         $(this).addClass('specifications__card-img--active')
+//     })
+// })
